148b16d0012dfd8a5e64ba0f3833a87709674500
Fuuma <masa81hiro69@gmail.com>
2023-03-23 01:28:43 +0900

    Add追加　スタイル手直し

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,8 +6,10 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import CheckIcon from "@mui/icons-material/Check";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { stagger, animate } from "framer-motion";
 import { useState } from "react";
+import { padding } from "@mui/system";
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -35,23 +37,33 @@ const Header = () => {
         </nav>
       </header>
       {openMenu && (
-        <div className='absolute left-0 w-48 bg-d9 shadow rounded'>
-          <div className='mx-4 my-2 cursor-pointer bg-white'>
+        <div className='flex flex-col left-0 w-48 h-screen bg-d9 shadow rounded'>
+          <div className='mx-4 my-2 cursor-pointer '>
             <VisibilityIcon sx={{ fontSize: "40px" }} />
             Plan
           </div>
-          <div className='px-4 py-2  cursor-pointer'>
-            <CheckIcon sx={{ fontSize: "40px" }}></CheckIcon>
-            Watched
+          <div className='mx-4 my-2 cursor-pointer '>
+            <CheckIcon sx={{ fontSize: "40px" }} />
+            Plan
           </div>
-          <div className='px-4 py-2  cursor-pointer'>
+          <div className='mx-4 my-2 cursor-pointer '>
             <CalendarTodayIcon sx={{ fontSize: "40px" }}></CalendarTodayIcon>
             Calender
           </div>
-          <div className='px-4 py-2  cursor-pointer'>
+          <div className='mx-4 my-2 cursor-pointer '>
             <SignalCellularAltIcon
               sx={{ fontSize: "40px" }}
             ></SignalCellularAltIcon>
+            Log
+          </div>
+          <div
+            className='mx-4 my-2 pr-10 cursor-pointer '
+            style={{ position: "absolute", bottom: 0 }}
+          >
+            <AddCircleOutlineIcon
+              sx={{ fontSize: "40px" }}
+            ></AddCircleOutlineIcon>
+            Add
           </div>
         </div>
       )}
