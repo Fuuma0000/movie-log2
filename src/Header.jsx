966e3d823a58a34632b12a44500310e3d87815da
Fuuma <masa81hiro69@gmail.com>
2023-03-23 03:04:02 +0900

    検索バーと送るボタンの追加

--- src/Header.jsx
+++ src/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
 
   return (
     <>
-      <header className='bg-d9 p-4'>
+      <header className='bg-d9 p-4 '>
         <nav className='flex'>
           <MenuIcon
             sx={{ fontSize: "40px" }}
@@ -30,34 +30,46 @@ const Header = () => {
           <div className='flex-1 pl-4'>
             <Title />
           </div>
-          <div className='space-x-4'>
+          <div>
+            <form class='flex w-full gap-2 md:max-w-md'>
+              <input
+                placeholder='Email'
+                class='w-full flex-1 rounded border border-white bg-indigo-400 px-3 py-2 text-white placeholder-indigo-100 outline-none ring-indigo-300 transition duration-100 focus:ring'
+              />
+
+              <button class='inline-block rounded bg-white px-8 py-2 text-center text-sm font-semibold text-indigo-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:text-indigo-600 md:text-base'>
+                Send
+              </button>
+            </form>
+          </div>
+          <div className='space-x-4 '>
             <HelpIcon sx={{ fontSize: "40px" }} />
             <SettingsIcon sx={{ fontSize: "40px" }} />
           </div>
         </nav>
       </header>
       {openMenu && (
-        <div className='flex flex-col left-0 w-48 h-screen bg-d9 shadow rounded'>
-          <div className='mx-4 my-2 cursor-pointer '>
+        <div className='flex flex-col w-48 h-screen bg-d9 shadow rounded'>
+          <div className='mx-4 my-4 cursor-pointer '>
             <VisibilityIcon sx={{ fontSize: "40px" }} />
             Plan
           </div>
-          <div className='mx-4 my-2 cursor-pointer '>
+          <div className='mx-4 my-4 cursor-pointer '>
             <CheckIcon sx={{ fontSize: "40px" }} />
             Plan
           </div>
-          <div className='mx-4 my-2 cursor-pointer '>
+          <div className='mx-4 my-4 cursor-pointer '>
             <CalendarTodayIcon sx={{ fontSize: "40px" }}></CalendarTodayIcon>
             Calender
           </div>
-          <div className='mx-4 my-2 cursor-pointer '>
+          <div className='mx-4 my-4 cursor-pointer '>
             <SignalCellularAltIcon
               sx={{ fontSize: "40px" }}
             ></SignalCellularAltIcon>
             Log
           </div>
           <div
-            className='mx-4 my-2 pr-10 cursor-pointer '
+            className='mx-4 my-2 cursor-pointer '
             style={{ position: "absolute", bottom: 0 }}
           >
             <AddCircleOutlineIcon
--- src/Title.jsx
+++ src/Title.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Title = () => {
-  return <div className='text-black first-line:text-4xl'>MovieLog</div>;
+  return <div className='text-black text-4xl'>MovieLog</div>;
 };
 
 export default Title;
