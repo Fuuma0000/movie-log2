94ecd465233ad03a190f17262e9f18582fd8fa54
Fuuma <masa81hiro69@gmail.com>
2023-03-25 03:33:09 +0900

    componentsに分けた

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Header from "./Header";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className='App'>
-      <Header></Header>
+      <Header />
       <body></body>
     </div>
   );
