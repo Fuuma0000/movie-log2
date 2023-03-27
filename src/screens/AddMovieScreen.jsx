c9af7cdd877c41d1b804628f64ba8d241177d1f2
Fuuma <masa81hiro69@gmail.com>
2023-03-28 00:47:26 +0900

    検索してないときに下まで色が表示されるように変更

--- src/screens/AddMovieScreen.jsx
+++ src/screens/AddMovieScreen.jsx
@@ -7,9 +7,12 @@ import { api_key } from "../APIkey";
 const AddMovieScreen = () => {
   const [fetchData, setFetchData] = useState([]);
   const ref = useRef();
+  //Postersを表示するかどうかの判定
+  const [submited, setSubmited] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSubmited(true);
 
     //APIURL
     const encoded = encodeURI(ref.current.value);
@@ -27,12 +30,12 @@ const AddMovieScreen = () => {
 
   const Posters = ({ fetchData }) => {
     return (
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 bg-main bottom-0 md:grid-cols-4 gap-4 pl-0 pt-16 pr-0 sm:pr-12 sm:pl-14'>
         {fetchData.map((data) => (
           <div className='px-6 py-4'>
             {/* 日付 */}
             <p className='font-bold text-gray-300 text-base mb-2 text-center'>
-              2022/03/23
+              {data.release_date}
             </p>
             {/* 画像 */}
             <img
@@ -43,15 +46,6 @@ const AddMovieScreen = () => {
             <p className='font-bold text-white text-xl mb-2 text-center'>
               {data.original_title}
             </p>
-            {/* 評価 */}
-            <div className='flex justify-center'>
-              <Rating
-                name='half-rating-read'
-                defaultValue={5}
-                precision={0.5}
-                readOnly
-              />
-            </div>
           </div>
         ))}
       </div>
@@ -59,25 +53,29 @@ const AddMovieScreen = () => {
   };
 
   return (
-    <div className='pl-0 pt-20 pr-0 sm:pr-12 sm:pl-14 z-0'>
-      <form onSubmit={(e) => handleSubmit(e)} className='h-20'>
-        <div className='relative rounded-md bg-gray-400 bg-opacity-25 w-2/5 mx-auto'>
-          {/* 横の虫眼鏡 */}
+    <div className='pl-0 pt-16 pr-0 sm:pr-12 sm:pl-14 z-0'>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className='h-20 flex flex-col justify-center items-center'
+      >
+        <div className='relative rounded-md bg-gray-400 bg-opacity-25 w-2/5'>
           <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
             <SearchIcon className='h-5 w-5 text-gray-400' />
           </div>
           <input
-            className='block w-full bg-transparent py-2 pl-10 pr-3 text-white placeholder-gray-300 focus:outline-none focus:placeholder-gray-400 focus:ring-0 sm:text-sm'
+            className='w-full bg-transparent py-2 pl-10 pr-3 text-white placeholder-gray-300 focus:outline-none focus:placeholder-gray-400 focus:ring-0 sm:text-sm'
             type='text'
             placeholder='映画を探す'
             ref={ref}
           />
         </div>
       </form>
-      <div className=''>
-        {/* <div className='grid grid-cols-1 sm:grid-cols-2 bg-main bottom-0 md:grid-cols-4 gap-4 pl-0 pt-16 pr-0 sm:pr-12 sm:pl-14'> */}
+      {/* SubMitedならPosetersを表示させる処理 */}
+      {submited ? (
         <Posters fetchData={fetchData} />
-      </div>
+      ) : (
+        <div className='fixed top-36 bottom-0 left-0 right-0 sm:pr-12 sm:left-14 bg-main'></div>
+      )}
     </div>
   );
 };
