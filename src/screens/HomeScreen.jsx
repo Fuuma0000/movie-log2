0a215de6278579e78c3765f3653e5437540d9734
Fuuma <masa81hiro69@gmail.com>
2023-03-31 18:17:40 +0900

    後でやる

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import Default from "./Default";
 
 const HomeScreen = () => {
   return (
     <>
-      <Default></Default>
-      {/* <div className='group pl-0 pt-16 pr-0 sm:pr-12 sm:pl-14'> */}
-      <div className='parentDiv hover:bg-gray-100 pl-0 pt-16 pr-0 sm:pr-12 sm:pl-14'>
-        <div className='childDiv opacity-100 hover:opacity-0 transition duration-500 ease-in-out'>
-          <p>子の div の文字</p>
-        </div>
+      <div className='text-white pl-0 pt-16 pr-0 sm:pr-12 sm:pl-14 z-0'>
+        あああああああああああ
       </div>
     </>
   );
