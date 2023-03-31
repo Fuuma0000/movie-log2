7423081ba49438bb191da05bb918ef6f59bdc302
Fuuma <masa81hiro69@gmail.com>
2023-03-31 18:15:48 +0900

    Default消した

--- src/screens/PlanScreen.jsx
+++ src/screens/PlanScreen.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Default from "./Default";
 import Movies from "./Movies";
 
 const PlanScreen = () => {
   return (
     <>
-      <Default></Default>
       <Movies></Movies>
     </>
   );
