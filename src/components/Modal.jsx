b6f22f00ece3bb3499d76ab567642f78170e37dd
Fuuma <masa81hiro69@gmail.com>
2023-04-17 00:50:15 +0900

    日付の入力欄作成

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import dayjs from "dayjs";
+import { DatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const Modal = ({ modalData, setModalData }) => {
+  const [value, setValue] = useState(dayjs(modalData.release_date));
+
+  const handleDateChange = (date) => {
+    setValue(date);
+  };
+
   return (
-    <div className='fixed top-0 left-0 w-full h-full bg-black flex items-center justify-center'>
+    <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 flex items-center justify-center'>
       {/* モーダルのコンテンツ */}
-      <div className='flex flex-col items-center justify-center w-1/2 p-1 bg-white'>
+      <div className='flex flex-col items-center justify-center w-1/2 p-1 bg-sub'>
         <img
-          className='h-96 '
+          className='h-96'
           src={`https://image.tmdb.org/t/p/w1280${modalData.poster_path}`}
-        ></img>
-        <h2>{modalData.release_date}</h2>
+        />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker value={value} onChange={handleDateChange} />
+        </LocalizationProvider>
+
+        <h2>{modalData.original_title}</h2>
         <button
           className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full'
           onClick={() => setModalData(null)}
