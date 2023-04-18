0f6b42df9c9e77a5b0a5a23b5a1c7579aedb84d6
Fuuma <masa81hiro69@gmail.com>
2023-04-18 18:44:08 +0900

    タイトルと星の実装

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useState } from "react";
+import TextField from "@mui/material/TextField";
 import dayjs from "dayjs";
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Rating from "@mui/material/Rating";
 
 const Modal = ({ modalData, setModalData }) => {
-  const [value, setValue] = useState(dayjs(modalData.release_date));
+  const [dateValue, setDateValue] = useState(dayjs(modalData.release_date));
+  const [rateValue, setRateValue] = useState(5);
+  const [nameValue, setNameValue] = useState(modalData.original_title);
 
   const handleDateChange = (date) => {
-    setValue(date);
+    setDateValue(date);
   };
 
   const darkTheme = createTheme({
@@ -27,17 +31,27 @@ const Modal = ({ modalData, setModalData }) => {
           className='h-96'
           src={`https://image.tmdb.org/t/p/w1280${modalData.poster_path}`}
         />
-        {/* ThemeProviderでMUIテーマを提供 */}
+        <ThemeProvider theme={darkTheme}>
+          <TextField required label='Required' value={nameValue} />
+        </ThemeProvider>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <ThemeProvider theme={darkTheme}>
             <DatePicker
-              value={value}
+              value={dateValue}
               onChange={handleDateChange}
               className='bg-date-picker border-cyan-300'
             />
           </ThemeProvider>
         </LocalizationProvider>
-
+        <ThemeProvider theme={darkTheme}>
+          <Rating
+            value={rateValue}
+            precision={0.5}
+            onChange={(event, value) => {
+              setRateValue(value);
+            }}
+          />
+        </ThemeProvider>
         <h2>{modalData.original_title}</h2>
         <button
           className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full'
